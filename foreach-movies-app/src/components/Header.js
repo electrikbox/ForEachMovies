53fa35cd693d0080a61ce3761d016310677058a4
electrikbox <electrikbox@icloud.com>
2024-04-15 20:31:46 +0200

    add header and footer components

---
+++ foreach-movies-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <img src="./logo.svg" className="App-logo" alt="logo" />
+    </header>
+  );
+};
+
+export default Header;
