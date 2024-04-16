ab8193785f61a3b53104255449897aff074ec7da
electrikbox <electrikbox@icloud.com>
2024-04-16 12:09:30 +0200

    add doc + search bar

--- foreach-movies-app/src/components/Header.js
+++ foreach-movies-app/src/components/Header.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 
+/**
+ * Header component.
+ * @returns {JSX.Element} The rendered header component.
+ */
 const Header = () => {
+  const [query, setQuery] = useState('');
+
   return (
     <header className="App-header">
       <img src="./logo.svg" className="App-logo" alt="logo" />
+      <div>
+        <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
+        <Link to={`/search-results?query=${query}`}><button>Search</button></Link>
+      </div>
     </header>
   );
 };
