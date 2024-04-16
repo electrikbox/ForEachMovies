2782448ba3d9f4ff79d8a9f530df127aeb1563c0
electrikbox <electrikbox@icloud.com>
2024-04-16 12:12:55 +0200

    update routes + fix decrepît root/app creation

--- foreach-movies-app/src/index.js
+++ foreach-movies-app/src/index.js
@@ -1,35 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
-import ErrorPage from './pages/ErrorPage';
+
 import Home from './pages/Home';
-import Movies from './pages/Movies';
+import ErrorPage from './pages/ErrorPage';
 import MovieDetail from './pages/MovieDetail';
 import MoviesGenre from './pages/MoviesGenre';
+import SearchResults from './pages/SearchResults';
+
 import './css/style.css';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/moviedetail" element={<MovieDetail />} />
-        <Route path="/moviesgenre" element={<MoviesGenre />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </BrowserRouter>
-    <Footer />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/moviedetail" element={<MovieDetail />} />
+          <Route path="/moviesgenre" element={<MoviesGenre />} />
+          <Route path="/search-results" element={<SearchResults />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+createRoot(document.getElementById('root')).render(<App />);
 reportWebVitals();
