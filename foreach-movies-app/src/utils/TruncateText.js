2661dbdf3352f34e24c268f98614d0af43fb20dd
electrikbox <electrikbox@icloud.com>
2024-04-16 13:16:50 +0200

    add doc and classname for style

--- foreach-movies-app/src/utils/TruncateText.js
+++ foreach-movies-app/src/utils/TruncateText.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+/**
+ * Truncates the given text if it exceeds the specified maximum length.
+ *
+ * @param {string} props.text - The text to be truncated.
+ * @param {number} props.maxLength - The maximum length of the truncated text.
+ * @returns {JSX.Element} The truncated text wrapped in a paragraph element.
+ */
 function TruncateText({ text, maxLength }) {
   const truncatedText = text.length > maxLength ? `${text.slice(0, maxLength)}...` : text;
 
   return (
-    <p>{truncatedText}</p>
+    <p className='movie-desc'>{truncatedText}</p>
   );
 }
 
