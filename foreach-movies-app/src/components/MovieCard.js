5e68635af1b443152ee7925d7b1a301dab13b159
electrikbox <electrikbox@icloud.com>
2024-04-16 14:05:11 +0200

    add original title

--- foreach-movies-app/src/components/MovieCard.js
+++ foreach-movies-app/src/components/MovieCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TruncateText from '../utils/TruncateText';
+import { Link } from 'react-router-dom';
 
 
 /**
@@ -12,7 +13,8 @@ import TruncateText from '../utils/TruncateText';
 const MovieItem = ({ movie }) => {
   return (
     <li key={movie.id} className='movie-card'>
-      <h3 className='movie-title'>{movie.title}</h3>
+      <h3 className='movie-title'>{movie.original_title}</h3>
+      <h5>({movie.title})</h5>
       <img
         src={movie.poster_path ? `https://image.tmdb.org/t/p/w300${movie.poster_path}` : 'no-poster.jpg'}
         alt={movie.title}
@@ -21,6 +23,7 @@ const MovieItem = ({ movie }) => {
         text={movie.overview ? movie.overview : 'No description available...'}
         maxLength={150} />
       {movie.vote_count > 0 ? <p>{Number(movie.vote_average).toFixed(1)}/10</p> : <p>no note</p>}
+      <Link to={`/moviedetail/${movie.id}`}>Details</Link>
     </li>
   );
 };
