3bfa64c66e3aa4354927a6f627ff640810c041e0
electrikbox <electrikbox@icloud.com>
2024-04-17 19:40:47 +0200

    add persistance for redux

--- foreach-movies-app/src/index.js
+++ foreach-movies-app/src/index.js
@@ -4,14 +4,16 @@ import { createRoot } from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux';
-import store from './store';
+import { persistor, store } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './css/style.css';
 
-
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
 reportWebVitals();
