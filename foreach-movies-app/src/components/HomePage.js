5efcf0d83584983cbe2bc1fb9ae59a6f1192116b
electrikbox <electrikbox@icloud.com>
2024-04-17 19:36:41 +0200

    remove search bar from header and place it in main section

--- foreach-movies-app/src/components/HomePage.js
+++ foreach-movies-app/src/components/HomePage.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import useHandleSearch from '../utils/SearchUrils';
 
 
 const Home = () => {
+  const { query, setQuery, error, handleSearch, handleKeyPress } = useHandleSearch();
+
   return (
     <main>
       <h1>Home</h1>
+      <div className='search-bar-home'>
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyDown={handleKeyPress}
+            placeholder="Search for movies..." />
+          <button type="button" onClick={handleSearch}>Search</button>
+        </div>
+        {error && <p>{error}</p>}
     </main>
   );
 };
