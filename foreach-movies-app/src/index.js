51f0cdad50904809ce7c21ec9a5c949b18f4dc3c
electrikbox <electrikbox@icloud.com>
2024-04-18 19:18:21 +0200

    remove redux and add react query

--- foreach-movies-app/src/index.js
+++ foreach-movies-app/src/index.js
@@ -1,19 +1,8 @@
 import App from './App';
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-import { Provider } from 'react-redux';
-import { persistor, store } from './store';
-import { PersistGate } from 'redux-persist/integration/react';
-
 import './css/style.css';
 
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
-);
+createRoot(document.getElementById('root')).render(<App />);
 reportWebVitals();
