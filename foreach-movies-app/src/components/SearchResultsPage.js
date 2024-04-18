35d1a1583268030955598b97186f67ab28c1baa1
electrikbox <electrikbox@icloud.com>
2024-04-18 10:58:07 +0200

    use query instead of redux

--- foreach-movies-app/src/components/SearchResultsPage.js
+++ foreach-movies-app/src/components/SearchResultsPage.js
@@ -1,44 +1,59 @@
 import Pagination from './Pagination';
 import MovieCard from './MovieCard';
-import { store } from '../store';
-import { useSelector } from 'react-redux';
-import { useSearchParams } from 'react-router-dom';
-import { getMovies } from '../actions/movies.action';
-import { useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const SearchResultsPage = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const [error, setError] = useState('');
-  const movies = useSelector((state) => state.movies);
+  const [movies, setMovies] = useState([]);
+  const [total_pages, setTotalPages] = useState(0);
+  const [page, setPage] = useState(0);
+  const navigate = useNavigate();
 
-  const handlePageChange = ({ selected }) => {
+  const onPageChange = ({ selected }) => {
     const nextPage = selected + 1;
     const query = searchParams.get('movie');
-    store.dispatch(getMovies(query, nextPage))
-      .then(() => {
-        setSearchParams({ movie: query, page: nextPage });
-      })
-      .catch((error) => {
-        setError('Error fetching movies.');
-        console.error('Error fetching movies:', error);
-      });
+    axios.get(`https://api.themoviedb.org/3/search/movie?include_adult=false`, {
+      params: {
+        api_key: API_KEY,
+        query: query,
+        page: nextPage,
+      }
+    })
+    .then((response) => {
+      setMovies(response.data.results);
+      setTotalPages(response.data.total_pages)
+      setPage(response.data.page);
+      window.scrollTo(0, 0);
+    })
+    .catch((error) => {
+      setError('Failed to fetch detail result.');
+      console.error('Error fetching movies:', error);
+    });
   };
+
+  useEffect(() => {
+    onPageChange({ selected: 0 });
+  }, [searchParams, navigate]);
 
   return (
     <main>
+      {error && <p>{error}</p>}
       <div className='search-result'>
         <ul>
-          {movies.results.map((movie) => (
+          {movies.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
           ))}
         </ul>
       </div>
-      {error && <p>{error}</p>}
       <Pagination
-        totalPages={movies.total_pages}
-        currentPage={movies.page}
-        onPageChange={handlePageChange} />
+          totalPages={total_pages}
+          currentPage={page}
+          onPageChange={onPageChange} />
     </main>
   );
 };
