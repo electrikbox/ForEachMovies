9e4c9832e19f555457462fd6d83de424c10c5b8e
electrikbox <electrikbox@icloud.com>
2024-04-21 02:20:00 +0200

    add doc and change list order + doc

--- foreach-movies-app/src/components/filters/orderFilter.jsx
+++ foreach-movies-app/src/components/filters/orderFilter.jsx
@@ -2,16 +2,24 @@ import { useState } from 'react';
 import { useSearchParams } from "react-router-dom";
 
 
+/**
+ * Represents a component for selecting the order of movies.
+ *
+ * @param {Function} props.onOrderSelect - The function to be called when the order is selected.
+ * @returns {JSX.Element} The rendered OrderFilter component.
+ */
 const OrderFilter = ({ onOrderSelect }) => {
   const [searchParams] = useSearchParams();
   const [selectedOrder, setOrder] = useState(searchParams.get('order') || 'popularity.desc');
 
+  // Represents the order list for filtering movies.
   const [orderList] = useState({
-    'Title A-Z': 'title.asc',
     'Popularity': 'popularity.desc',
+    'Title A-Z': 'title.asc',
     'Note': 'vote_average.desc',
   });
 
+  // Handles the change event of the order select input.
   const handleOrderChange = (event) => {
     const selectedOrder = event.target.value;
     setOrder(selectedOrder);
