da702580efdf997859b64e0231bea8d359754c3c
electrikbox <electrikbox@icloud.com>
2024-04-21 02:20:29 +0200

    remove axios request and change ir to useQuery + doc

--- foreach-movies-app/src/components/MovieDetailPage.jsx
+++ foreach-movies-app/src/components/MovieDetailPage.jsx
@@ -1,55 +1,61 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { getMovieDetail } from '../utils/requests';
+import { useEffect } from 'react';
 
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-
+/**
+ * Renders the movie detail page.
+ *
+ * @returns {JSX.Element} The movie detail page component.
+ */
 const MovieDetail = () => {
-  const [movie, setMovie] = useState({});
-  const [error, setError] = useState(null);
   const param = useParams();
   const navigate = useNavigate();
 
+  // Fetches the movie detail and stores the result in the `data` state variable.
+  const { status, data, error, refetch, isFetching } = useQuery(
+    ['movies'],
+    () => getMovieDetail(param.id),
+    {
+      staleTime: 60_000,
+      cacheTime: 60_000,
+      enabled: true,
+    }
+  );
+
   useEffect(() => {
-    axios.get(`https://api.themoviedb.org/3/movie/${param.id}`, {
-      params: {
-        api_key: API_KEY,
-      }
-    })
-      .then((response) => {
-        setMovie(response.data);
-      })
-      .catch((error) => {
-        setError(error);
-        throw new Error('Failed to fetch detail result.');
-      });
+    refetch()
   }, [param.id]);
+
+  if (error) return <p className='error'>An error has occurred</p>;
 
   return (
     <main>
-      {error && <p>Failed to fetch detail result.</p>}
-      {!error &&
-      <div className='movie-detail'>
-        <img
-          src={movie.poster_path ? `https://image.tmdb.org/t/p/w400${movie.poster_path}` : '/no-poster.jpg'}
-          alt={movie.title}
-        />
-        <h2>{movie.original_title}</h2>
-        <h3>({movie.title})</h3>
-        <p>{movie.overview}</p>
-        <p>{movie.vote_average}/10</p>
-        <p>{movie.release_date}</p>
-        <p>Genres:</p>
-        {movie.genres && (
-          <ul>
-            {movie.genres.map((genre) => (
-              <a href={`/moviesgenre?genre=${genre.id}&page=1`}><li key={genre.id}>{genre.name}</li></a>
-            ))}
-          </ul>
-        )}
-        <button onClick={() => navigate(-1)}>Go back</button>
-      </div> }
+      {status === 'loading' || isFetching ? <div className="loader"></div> :
+        <>
+          <h2 className='detail-title'>{data.title}</h2>
+          <p className='original-title'><strong>Original title :</strong> {data.original_title}</p>
+          <div className='movie-detail'>
+            <img className='detail-pic'
+              src={data.poster_path ? `https://image.tmdb.org/t/p/w400${data.poster_path}` : '/no-poster.jpg'}
+              alt={data.title}
+            />
+            <div className='detail-content'>
+              <p className='overview'>{data.overview}</p>
+              <p>{Math.round(Number(data.vote_average))}/10</p>
+              <p>{data.release_date}</p>
+              {data.genres && (
+                <ul className='genres'>
+                  {data.genres.map((genre) => (
+                    <a href={`/moviesgenre?genre=${genre.id}&page=1`}><li key={genre.id}>{genre.name}</li></a>
+                  ))}
+                </ul>
+              )}
+              <button className="back" onClick={() => navigate(-1)}>Go back</button>
+            </div>
+          </div>
+        </>}
     </main>
   );
 };
