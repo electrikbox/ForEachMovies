3c42bda1ebb2ce41597e813c3638d24ca6965e72
electrikbox <electrikbox@icloud.com>
2024-04-21 02:19:31 +0200

    update handle genre change + doc

--- foreach-movies-app/src/components/filters/GenresFilter.jsx
+++ foreach-movies-app/src/components/filters/GenresFilter.jsx
@@ -1,23 +1,30 @@
 import { getMoviesGenres } from "../../utils/requests";
 import { useQuery } from 'react-query';
 import { useState } from 'react';
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 
+/**
+ * Renders a dropdown menu for selecting a movie genre.
+ *
+ * @param {Function} props.onGenreSelect - The callback function to be called when a genre is selected.
+ * @returns {JSX.Element} The GenreMenu component.
+ */
 const GenreMenu = ({onGenreSelect}) => {
   const [searchParams] = useSearchParams();
-  const [genre, setGenre] = useState(searchParams.get('genre') || '');
-  const navigate = useNavigate();
+  const [selectedYear, setGenre] = useState(searchParams.get('genre') || '');
 
+  // Fetches the movie genres and stores the result in the `data` state variable.
   const { status, data, error } = useQuery(
     ['genre'],
     () => getMoviesGenres()
   );
 
-  const handleGenre = (e) => {
-    onGenreSelect(e.target.value)
-    setGenre(e.target.value);
-    navigate(`/moviesgenre?genre=${e.target.value}&page=1`);
+  // Handles the change event of the genre select input.
+  const handleGenreChange = (event) => {
+    const selectedGenre = event.target.value;
+    setGenre(selectedGenre);
+    onGenreSelect(selectedGenre)
   }
 
   if (error) return <p>"An error has occurred"</p>;
@@ -25,7 +32,7 @@ const GenreMenu = ({onGenreSelect}) => {
 
   return (
     <div className="select">
-      <select selected className="format" name="genre-menu" id="genre-menu" onChange={handleGenre} value={genre}>
+      <select selected className="format" name="genre-menu" id="genre-menu" onChange={handleGenreChange} value={selectedYear}>
         <option value="" disabled>Choose a genre...</option>
         {data.genres.map((genre) => (
           <option key={genre.id} value={genre.id}>{genre.name}</option>
